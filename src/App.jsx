256709d5a1e42e03c639402f050651e33530a1e7
Kanghee <95953304+rivxrhxx@users.noreply.github.com>
2025-09-10 18:25:49 +0900

    feat: 버튼 컴포넌트 인풋컴포넌트 추가완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Button } from "./components/button";
+import { Input } from "./components/input";
 
 function App() {
   return (
     <div style={{ padding: "40px", maxWidth: "300px", margin: "0 auto" }}>
       <h1>컴포넌트 미리보기</h1>
       <Button /> {}
+      <Input />
+      {}
     </div>
   );
 }
---
+++ src/components/input.jsx
@@ -0,0 +1,30 @@
+import styled from "@emotion/styled";
+
+export const Input = () => {
+  return (
+    <Container>
+      <Text>아이디</Text>
+      <InputStyle placeholder="아이디를 입력" />
+    </Container>
+  );
+};
+
+const InputStyle = styled.input`
+  border-radius: 6px;
+  width: 100%;
+  height: 52px;
+  font-size: 18px;
+  padding: 0 12px;
+  border: 1px solid #000;
+`;
+
+const Text = styled.p`
+  font-weight: bold;
+  font-size: 20px;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
