c075c6a23f8c43c8504a9252455348ef8d259a3f
kimjihwan8 <kimjihwan1592@naver.com>
2025-09-24 21:33:21 +0900

    feat: detail page 퍼블리싱

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,11 +1,60 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from 'axios';
+import styled from '@emotion/styled';
 
+type Product = {
+  id: number;
+  title: string;
+  content: string;
+  author: {
+    id: number;
+    username: string;
+    role: string;
+  };
+};
 
 export default function Detail() {
 
+  const { id } = useParams();
+
+  let [data, setData] = useState<Product[]>([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000');
+        console.log(response.data)
+        setData(response.data);
+      } catch (error) {
+        console.error("오류:", error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const datas = data.find((x) => x.id === Number(id));
+  console.log(datas)
+
   return (
     <>
-      <h1>{a.title}</h1>
-      <h3>{a.content}</h3>
+      <Div>
+        <Title>{datas?.title}</Title>
+        <Contents>{datas?.content}</Contents>
+      </Div>
     </>
   )
 }
+
+const Div = styled.div`
+  padding: 0 400px;
+`
+
+const Title = styled.p`
+  font-size:50px;
+  font-weight:600;
+`
+
+const Contents = styled.p`
+  font-size: 30px;
+`
